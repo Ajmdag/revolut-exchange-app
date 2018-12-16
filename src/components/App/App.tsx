7b857cbb3e59c46e64306883cafd03f47764178b
Yury <yurka7321@gmail.com>
2018-12-16 16:30:03 +0300

    Add tslint

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React, {Component} from 'react'
 
 class App extends Component {
-  render() {
-    return <div className="App">Hello Revolut</div>;
+  public render() {
+    return <div className="App">Hello Revolut!</div>
   }
 }
 
-export default App;
+export default App
